f285bef7baf6fe3e8957ec0906b4c0a5c38f5000
Vados61 <pvild3d@gmail.com>
2022-09-07 18:46:13 +0300

    third version 05 (broken version)

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-'''
+"""
 1. нужно выбрать и сделать импорт модуля для работы с ботом
 2. сделать нормальные теги
 3. сделать нормальные имена лекторов и увеличить их колличество
 4. реализовать логику диалога бота с пользователем
-'''
+"""
+
 import random
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.utils import executor
 
 from data import *
 
+
+class UserState(StatesGroup):
+    name = State()
+    change_tags = State()
+    change_lector = State()
+
+
 TOKEN = "5562333620:AAFEDZ2_5EWbPOWPEbrZwVKlfOo-X0MRYmw"
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    print('1')
+@dp.message_handler(commands=['reg'])
+async def user_register(message: types.Message):
+    await message.answer("Введите своё имя")
+    await UserState.name.set()
+
+
+@dp.message_handler(state=UserState.name)
+async def get_username(message: types.Message, state: FSMContext):
+    await state.update_data(username=message.text)
     answer = "Введите через пробел три предпочтительных навыка:"
     for id, item in enumerate(lector_tags):
         answer += f'\n{id + 1}. {item}'
     await message.answer(answer)
+    await UserState.next()
 
 
-@dp.message_handler()
-async def send_matching_result(message: types.Message):
-    tags = set(message.text.split(' '))
+@dp.message_handler(state=UserState.change_tags)
+async def change_tags(message: types.Message, state: FSMContext):
+    user_tags = set(message.text.split())
+    await state.update_data(usertags=user_tags)
     answer = 'По вашим критериям подходят следущие лекторы:'
     for lector in lectors:
-        if lector.cheak_matching(tags):
-            answer += lector.cheak_matching(tags)
+        if lector.cheak_matching(user_tags):
+            answer += lector.cheak_matching(user_tags)
     await message.answer(answer)
+    await UserState.next()
 
 
 class Student:
@@ -57,13 +77,6 @@ class Lector:
 
 
 lectors = [Lector(lector, random.sample(lector_tags, 2)) for lector in lector_list]
-
-# tegs = set(random.sample(lector_tegs, 3))
-#
-# for lector in lectors:
-#
-#     if lector.cheak_matching(tegs):
-#         print(lector.cheak_matching(tegs))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
